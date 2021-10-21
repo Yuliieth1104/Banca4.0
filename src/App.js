27b4334a71b0a5335267ad319ac629384eded43e
JennyJ-B2608 <alejajb1605@gmail.com>
2021-10-21 15:52:00 -0500

    g

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import Slider from './componentes/Slider'
-import './estilos.css';
-import styled from 'styled-components';
+import Index from './Components/Index'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Roles from './Components/Roles/Roles';
+import Director from './Components/Director/Director'
+import Gerente from './Components/Gerente/Gerente'
+import Asesor from './Components/Asesor/Asesor'
+import Cajero from './Components/Cajero/Cajero'
 
-
-const App = () => {
+function App() {
   return (
-    <main>
-      <Titulo>Banca</Titulo>
-      <Slider />
-    </main>
+    <Router>
+    <Switch>
+          <Route path="/home">
+          <Index />
+          </Route>
+          <Route path="/Roles">
+          <Roles />
+          </Route>
+          <Route path="/Director">
+          <Director />
+          </Route>
+          <Route path="/Gerente">
+          <Gerente />
+          </Route>
+          <Route path="/Asesor">
+          <Asesor />
+          </Route>
+          <Route path="/Cajero">
+          <Cajero />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
-
-const Titulo = styled.p `
-  font-size: 18px;
-  font-weight: 700;
-  text-transform: uppercase;
-  margin-bottom: 10px;
-`;
 
 export default App;
