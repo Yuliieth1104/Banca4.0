ae6ac769e61aaba879779f0b7258985a0d4c8b19
unknown <SENA@PALDFPCBIFSD226.FormacionSena.RED>
2021-10-21 15:57:11 -0500

    h

---
+++ src/Components/Asesor/Asesor.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { useHistory } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+const Asesor = () => {
+
+    let History  = useHistory();
+
+    return (
+        <div>
+            <Button onClick = {()=>History.push('/home')}>Inicio</Button>
+            <br/>
+            <br/>
+            <Button onClick = {()=>History.push('/Roles')}> Regresar</Button>
+        </div>
+    )
+}
+
+export default Asesor
