daa7cc0fa2cac1530c9526a53b1970f3314e48ea
Kelly Yulieth <90258809+Yuliieth1104@users.noreply.github.com>
2021-10-29 17:03:53 -0500

    Add files via upload

--- src/Components/Asesor/Asesor.js
+++ src/Components/Asesor/Asesor.js
@@ -3,26 +3,28 @@ import Button from 'react-bootstrap/Button'
 import { useHistory } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import PersonaNatural from './PersonaNatural';
 
 
-const Asesor = () => {
+function Asesor() {
 
-    let History  = useHistory();
+    let History = useHistory();
 
     return (
         <div>
-            <Button onClick = {()=>History.push('/')}>Inicio</Button>
-            <br/>
-            <br/>
-            <Button onClick = {()=>History.push('/Roles')}> Regresar</Button>
-            <br/>
-            <br/>
-            <Button onClick = {()=>History.push('/PersonaNatural')}>Persona Natural</Button>
-            <br/>
-            <br/>
-            <Button onClick = {()=>History.push('/PersonaJuridica')}>Persona Juridica</Button>
+            <PersonaNatural/>
+            <Button onClick={() => History.push('/')}>Inicio</Button>
+            <br />
+            <br />
+            <Button onClick={() => History.push('/Roles')}> Regresar</Button>
+            <br />
+            <br />
+            <Button onClick={() => History.push('/PersonaNatural')}>Persona Natural</Button>
+            <br />
+            <br />
+            <Button onClick={() => History.push('/PersonaJuridica')}>Persona Juridica</Button>
         </div>
-    )
+    );
 }
 
 export default Asesor
